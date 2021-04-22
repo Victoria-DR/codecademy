c4c63f74f9732998ddb8be201fea1b297738a657
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2021-04-21 20:45:42 -0400

    Complete hard challenge level

--- forum-challenges/tolerant_string_matching.py
+++ forum-challenges/tolerant_string_matching.py
@@ -9,42 +9,54 @@ def basicMatch(message, snippet):
 
 def intermediateMatch(message, snippet, decodings):
     # Render case insensitive
-    message = message.upper()
+    message_alter = message.upper()
     snippet = snippet.upper()
     # Check if snippet is a substring of message
-    if snippet in message:
+    if snippet in message_alter:
         return True
 
     # Convert decodings into a dictionary
     decoding_dict = {x[0]: x[1] for x in decodings}
 
     # Find first occurence of first character in snippet
-    start = message.find(snippet[0])
-    if start == -1:
-        # Determine if the first character of snippet is encoded in message
-        if snippet[0] not in decoding_dict.values():
-            return False
-        else:
-            # Decode the first character of snippet in message
-            for coded, decoded in decoding_dict.items():
-                if decoded == snippet[0]:
-                    message = message.replace(coded, decoded, 1)
-                    # Find new first occurence of first character in snippet
-                    start = message.find(snippet[0])
+    start = message_alter.find(snippet[0])
+    # Determine if the first character of snippet is not encoded in message
+    if (start == -1) and (snippet[0] not in decoding_dict.values()):
+        return False
+
+    # Find all possiblilities of decoding the first character in snippet
+    possible_start_decodings = {}
+    for coded, decoded in decoding_dict.items():
+        if (decoded == snippet[0]) and (message_alter.find(decoded) <= start):
+            possible_start_decodings[coded] = None
+    
+    earliest = []
+    # Find earliest occurence of any possibile decoding
+    for possibility in possible_start_decodings:
+        possible_start_decodings[possibility] = message_alter.find(possibility)
+        if (earliest == []) or (earliest[1] < possible_start_decodings[possibility]):
+            earliest = [possibility, possible_start_decodings[possibility]]
+
+    if earliest:
+        # Decode first character of snippet in message
+        message_alter = message_alter.replace(earliest[0], snippet[0], 1)
+        # Find new first occurence of first character in snippet
+        start = message_alter.find(snippet[0])
 
     matches = False
     index = 1
     # Run while snippet is not yet determined to be in message
-    while (not matches) and (start <= len(message) - len(snippet)):
+    while (not matches) and (start <= len(message_alter) - len(snippet)):
         # Check if letter is the same in message and snippet
-        if message[start + index] == snippet[index]:
+        if message_alter[start + index] == snippet[index]:
             index += 1
         else:
             # Decode a character
             char_decoded = False
             for coded, decoded in decoding_dict.items():
-                if coded == message[start + index: start + index + len(coded)]:
-                    message = message[:start + index] + message[start + index:].replace(coded, decoded, 1)
+                if coded == message_alter[start + index: start + index + len(coded)]:
+                    message_alter = message_alter[:start + index] + message_alter[start + index:].replace(coded, decoded, 1)
+                    # Allow for decoding to occur only once
                     index += 1
                     char_decoded = True
                     break
@@ -54,8 +66,7 @@ def intermediateMatch(message, snippet, decodings):
 
         # Determine if snippet is a substring of message
         if index == len(snippet):
-            print(message, snippet)
-            matches = True if (snippet.upper() in message.upper()) else False
+            matches = True if (snippet.upper() in message_alter.upper()) else False
             break
 
     return matches
@@ -69,4 +80,68 @@ def intermediateMatch(message, snippet, decodings):
 # print(intermediateMatch("Co%67eca%67emy is great", "co%67ecademy", [ ["%67", "D"]]))
 # print(intermediateMatch("Codecademy is great", "codec%65demy", [["%65", "A"]]))
 # print(intermediateMatch("abc%6efg", "abcdefg", [["e", "d"], ["%6", "e"]]))
-    
+
+def hardMatch(message, snippet, decodings):
+    # Render case insensitive
+    message_alter = message.upper()
+    snippet = snippet.upper()
+    # Check if snippet is a substring of message
+    if snippet in message_alter:
+        return True
+
+    # Convert decodings into a dictionary
+    decoding_dict = {x[0]: x[1] for x in decodings}
+
+    # Find first occurence of first character in snippet
+    start = message_alter.find(snippet[0])
+    # Determine if the first character of snippet is not encoded in message
+    if (start == -1) and (snippet[0] not in decoding_dict.values()):
+        return False
+
+    # Find decoding path to first character in snippet
+    path = [snippet[0]]
+    added_new = True
+    while (path[-1] not in message_alter[:message_alter.find(path[-1])]) and added_new:
+        added_new = False
+        # Add new point along path
+        for coded, decoded in decoding_dict.items():
+            if decoded == path[-1]:
+                path.append(coded.upper())
+                added_new = True
+
+    # Decode first character of snippet in message
+    if path[-1] in message_alter:
+        message_alter = message_alter.replace(path[-1], path[0], 1)
+        # Find new first occurence of first character in snippet
+        start = message_alter.find(snippet[0])
+
+    matches = False
+    index = 1
+    # Run while snippet is not yet determined to be in message
+    while (not matches) and (start <= len(message_alter) - len(snippet)):
+        # Check if letter is the same in message and snippet
+        if message_alter[start + index] == snippet[index]:
+            index += 1
+        else:
+            # Decode a character
+            char_decoded = False
+            for coded, decoded in decoding_dict.items():
+                if coded == message_alter[start + index: start + index + len(coded)]:
+                    message_alter = message_alter[:start + index] + message_alter[start + index:].replace(coded, decoded, 1)
+                    char_decoded = True
+                    break
+            # Repeat entire process with new start point
+            if not char_decoded:
+                return intermediateMatch(message[start + 1:], snippet, decodings)
+
+        # Determine if snippet is a substring of message
+        if index == len(snippet):
+            matches = True if (snippet.upper() in message_alter.upper()) else False
+            break
+
+    return matches
+
+# print(hardMatch("Codecademy is great", "codecademy", []))
+# print(hardMatch("Codec%65demy is great", "codecademy", [["%65", "A"]]))
+# print(hardMatch("Codec%65demy is great", "codecademy", [["%65", "$10"], ["$10", "A"]]))
+# print(hardMatch("Codec%65demy is great", "codecademy", [["%65", "$10"], ["$10", "B"]]))
